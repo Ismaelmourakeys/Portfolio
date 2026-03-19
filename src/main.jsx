5fd70467269ee0227564bd242bc911f58e3c01db
ismaelmourakeys <ismael_moura@outlook.com>
2026-03-19 00:30:59 -0300

    vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react"; // ← react, não /next
+import { Analytics } from "@vercel/analytics/react";
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <SpeedInsights />  {/* ← coloca fora do App, aqui mesmo */}
+    <Analytics />
+  </React.StrictMode>
+);
+
+/*createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
